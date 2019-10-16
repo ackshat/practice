2c17ab66ce4a54a36ede42ae1a861dc4253319ec
Alok Sharma <ackshat@gmail.com>
2019-10-16 18:22:36 +0530

    Update MinMaxInc1DecO1.java

--- 16-10-2019/MinMaxInc1DecO1.java
+++ 16-10-2019/MinMaxInc1DecO1.java
@@ -10,29 +10,37 @@ class MinMaxIncDO1{
       int f = fMap.getOrDefault(k,0);
       if(f>0){
         rMap.get(f).remove(k);
+        if(rMap.get(f).size()==0){
+          rMap.remove(f);
+          if(cMin==f)  cMin = f+1;
+        }
       }
-      rMap.getOrDefault(f+1, new HashSet<>()).add(k);
-      fMap.put(k,f+1);
       
+      rMap.getOrDefault(f+1, new HashSet<String>()).add(k);
+      fMap.put(k,f+1);
+     
       if(f+1> cMax) cMax = f+1;
       
     }
     
-    public void min(String k){
+    public void dec1(String k){
       int f = fMap.getOrDefault(k,0);
       if(f==0) return;
       
-      rMap.get(f).remove(k);
-      if(rMap.get(f).size()==0) rMap.remove(f);
-      
-      if(f-1 == 0 ){
+      if(f-1 == 0){
         fMap.remove(k);
       }
       else{
-      fMap.put(k,f-1);
-      rMap.getOrDefault(f-1,new HashSet<>()).add(k);
+        fMap.put(k,f-1);
+        rMap.getOrDefault(f-1, new HashSet<String>()).add(k);
       }
-      if (f-1 >0  && f-1 < cMin ) cMin = f-1;
+      rMap.get(f).remove(f);
+      if(rMap.get(f).size()==0){
+        rMap.remove(f);
+        if(cMax==f) cMax = f-1;
+      }
+      
+      if(cMin > f-1) cMin = f-1;
       
     }
     public Set<String> max(){
