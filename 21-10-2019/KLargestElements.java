ce054f45ff259b6a71113d24933199f8221771de
Alok Sharma <ackshat@gmail.com>
2019-10-22 10:46:59 +0530

    Update KLargestElements.java

--- 21-10-2019/KLargestElements.java
+++ 21-10-2019/KLargestElements.java
@@ -1,11 +1,22 @@
+package dsalgo;
 
-public class KLargestElements{
+import java.util.Arrays;
+
+public class KLargestElements {
+
   public static void main(String args[]){
-    
-    kLargest(new int[]{2,8,16,17,8,15,14,9},4);
+    int[] a = new int[]{2,8,16,17,8,15,14,9};
+    System.out.println(Arrays.toString(a)+"\n");
+	 int k =  kLargest(a,4);
+	 
+	  for(int i = k;i< a.length;i++) {
+		  System.out.print(a[i] + ", ");
+	  }
+	  System.out.println("\n"+Arrays.toString(a));
   }
-  public static int[] kLargest(int a[], int k){
-    System.out.println(kLargestUsingPartition(a,k,0,a.length-1));
+  
+  public static int kLargest(int a[], int k){
+    return kLargestUsingPartition(a,k,0,a.length-1);
   }
 
   
@@ -15,21 +26,24 @@ public class KLargestElements{
     
     if(lo<hi){ 
       int left = lo;
-      int right = hi;
+      int right = hi-1;
       while(left<right){
         while(a[left]<a[pivot]) left++;
         while(a[right]>a[pivot]) right--;
-        swap(a,left,right);
+        swap(a,left++,right--);
       }
+      if(left<pivot) {
        swap(a,left,pivot);
+      }
       
-      if(k==left+1)return left+1;
+      if(k==hi-left+1)return left+1;
       
-      if(K<left+1){
-        pivot = kLargestUsingPartition(a[],  k,lo, left);{
+      if(k<hi-left+1){
+    	  pivot = kLargestUsingPartition(a,  k,left+1, hi);
+        
       }
       else{
-         pivot = kLargestUsingPartition(a[],  k,left+1, hi);
+    	  pivot = kLargestUsingPartition(a,  k,lo, left);
       }
       
       
@@ -38,7 +52,7 @@ public class KLargestElements{
     return pivot;
   }
   
-  public static swap(int a[], int i, int j){
+  public static void swap(int a[], int i, int j){
     int t = a[i];
     a[i]=a[j];
     a[j]=t;
